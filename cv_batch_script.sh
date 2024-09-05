6a73aa9cbf820189bed2e5f30cd9746b0f410637
simon-smart88 <simon.smart@cantab.net>
2024-09-05 10:33:08 +0100

    increase RAM

--- cv_batch_script.sh
+++ cv_batch_script.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=MDG_cross_validation
 #SBATCH --array=1-5	    # Job array with 25 tasks i.e. 5 reps x 5 groups
 #SBATCH --time=01:30:00    # Time limit for each job (adjust as needed)
-#SBATCH --mem=8G           # Memory required per job (adjust as needed)
+#SBATCH --mem=16G           # Memory required per job (adjust as needed)
 #SBATCH --cpus-per-task=1  # Number of CPUs per job (adjust as needed)
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
